5a7da617f5fabddf61840dc3c8c97d6422c3d0c5
NischalCode-dev <nischal554433@gmail.com>
2026-05-08 18:03:28 +0545

    Added a new page

--- script.js
+++ script.js
@@ -1,17 +1,81 @@
-setTimeout(function() {
-    alert("Welcome to Foodie ridE! This website is created by Nischal kafle.It is in developing phase.");
-}, 3000); 
 
-function toggleMenu() {
-    let menu = document.getElementById("navMenu");
-    menu.classList.toggle("show");
-} 
-let click=document.getElementsByClassName(".menu-icon");
-window.addEventListener("load", function () {
+
+// LOADER
+
+window.addEventListener("load", () => {
+
     const loader = document.getElementById("loader");
+
     loader.style.opacity = "0";
 
     setTimeout(() => {
+
         loader.style.display = "none";
+
     }, 500);
+
+});
+
+// ALERT
+
+setTimeout(() => {
+
+    alert(
+        "Welcome to Foodie ridE 🚖🍕\nWebsite is currently in developing phase.This is created by Nischal Kafle"
+    );
+
+}, 3000);
+
+// MOBILE MENU
+
+function toggleMenu(){
+
+    let menu = document.getElementById("navMenu");
+
+    menu.classList.toggle("show");
+
+}
+
+// TYPING EFFECT
+
+const text = "Delicious Food, Delivered Fast 🍕";
+
+const typing = document.getElementById("typing");
+
+let i = 0;
+
+function typeText(){
+
+    if(i < text.length){
+
+        typing.innerHTML += text.charAt(i);
+
+        i++;
+
+        setTimeout(typeText,100);
+
+    }
+
+}
+
+typeText();
+
+// SCROLL ANIMATION
+
+const hiddenElements = document.querySelectorAll(".hidden");
+
+window.addEventListener("scroll",()=>{
+
+    hiddenElements.forEach((el)=>{
+
+        const top = el.getBoundingClientRect().top;
+
+        if(top < window.innerHeight - 100){
+
+            el.classList.add("show");
+
+        }
+
+    });
+
 });
